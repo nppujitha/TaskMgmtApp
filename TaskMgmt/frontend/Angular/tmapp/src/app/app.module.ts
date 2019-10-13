b8c729926c152fadfa2cbb70d88aaa64118c72a0
pujasparks7 <pujasparks7@gmail.com>
2019-10-13 10:24:41 -0500

    inital version

---
+++ TaskMgmt/frontend/Angular/tmapp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateComponent } from './employee/create/create.component';
+import { ReadComponent } from './employee/read/read.component';
+import { UpdateComponent } from './employee/update/update.component';
+import { DeleteComponent } from './employee/delete/delete.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
